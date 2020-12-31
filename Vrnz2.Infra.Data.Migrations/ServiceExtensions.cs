53158684145312bb7f75deefb2d44ceece388742
Vernizze <cfvernizze@gmail.com>
2020-12-31 15:28:26 -0300

    Ajuste no modelo de passagem de parametros

--- Vrnz2.Infra.Data.Migrations/ServiceExtensions.cs
+++ Vrnz2.Infra.Data.Migrations/ServiceExtensions.cs
@@ -1,18 +1,19 @@
 ﻿using FluentMigrator.Runner;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Vrnz2.Infra.Data.Migrations
 {
     public static class ServiceExtensions
     {
-        public static IServiceCollection ConfigSqLiteMigrations<T>(this IServiceCollection services, string databaseFullPath)
+        public static IServiceCollection ConfigSqLiteMigrations(this IServiceCollection services, Assembly assembly, string databaseFullPath)
         {
             services
                 .AddFluentMigratorCore()
                     .ConfigureRunner(rb => rb
                     .AddSQLite()
                     .WithGlobalConnectionString($"Data Source={databaseFullPath}")
-                    .ScanIn(typeof(T).Assembly).For.Migrations())
+                    .ScanIn(assembly).For.Migrations())
                 .AddLogging(lb => lb.AddFluentMigratorConsole());
 
             return services;
