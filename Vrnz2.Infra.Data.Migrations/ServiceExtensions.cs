a6d5aa23c4c6b456c5c082fbf0d883088b0bf2a9
Vernizze <cfvernizze@gmail.com>
2020-12-31 17:46:12 -0300

    Correcao de bug na criação do BD

--- Vrnz2.Infra.Data.Migrations/ServiceExtensions.cs
+++ Vrnz2.Infra.Data.Migrations/ServiceExtensions.cs
@@ -1,22 +1,33 @@
 ﻿using FluentMigrator.Runner;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Reflection;
 
 namespace Vrnz2.Infra.Data.Migrations
 {
     public static class ServiceExtensions
     {
-        public static IServiceCollection ConfigSqLiteMigrations(this IServiceCollection services, Assembly assembly, string databaseFullPath)
+        public static IServiceCollection ConfigSqLiteMigrations(this IServiceCollection services, Assembly assembly, string databasePath, string databaseName)
         {
+            if (!Directory.Exists(databasePath))
+                Directory.CreateDirectory(databasePath);
+
             services
                 .AddFluentMigratorCore()
                     .ConfigureRunner(rb => rb
                     .AddSQLite()
-                    .WithGlobalConnectionString($"Data Source={databaseFullPath}")
+                    .WithGlobalConnectionString($"Data Source={Path.Combine(databasePath, databaseName)}")
                     .ScanIn(assembly).For.Migrations())
                 .AddLogging(lb => lb.AddFluentMigratorConsole());
 
+            using (var scope = services.BuildServiceProvider().CreateScope())
+                UpdateDatabase(scope.ServiceProvider);
+
             return services;
         }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+            => serviceProvider.GetRequiredService<IMigrationRunner>().MigrateUp();
     }
 }
