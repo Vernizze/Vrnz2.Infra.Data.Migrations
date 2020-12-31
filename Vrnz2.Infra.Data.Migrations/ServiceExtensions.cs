3b9fe105b1172f7753cb7a0109ca61b0f9b7aa97
Vernizze <cfvernizze@gmail.com>
2020-12-31 10:41:28 -0300

    Criacao de configs de Migrations para SqLite

---
+++ Vrnz2.Infra.Data.Migrations/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vrnz2.Infra.Data.Migrations
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigSqLiteMigrations<T>(this IServiceCollection services, string databaseFullPath)
+        {
+            services
+                .AddFluentMigratorCore()
+                    .ConfigureRunner(rb => rb
+                    .AddSQLite()
+                    .WithGlobalConnectionString($"Data Source={databaseFullPath}")
+                    .ScanIn(typeof(T).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole());
+
+            return services;
+        }
+    }
+}
